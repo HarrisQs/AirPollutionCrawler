969d6d64993200195953e5290e2a0dc042f11f7e
HarrisQs <harris32916@gmail.com>
2016-05-15 14:48:29 +0800

    add some Auto script

---
+++ AutoCrawl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export  PATH=/bin:/sbin:/usr/bin:/usr/sbin
+cd /home/harris/Desktop/Crawl_PSI
+/usr/bin/python Crawl.py
---
+++ AutoPush.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export  PATH=/bin:/sbin:/usr/bin:/usr/sbin
+cd /home/harris/Desktop/Crawl_PSI
+/usr/bin/git add .
+/usr/bin/git commit -m date
+/usr/bin/git push
