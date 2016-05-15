0de2facb8444e9b781992bceea69b8f4430ce4b5
HarrisQs <harris32916@gmail.com>
2016-05-15 16:25:43 +0800

    Sun May 15 16:25:43 CST 2016 Update Data

--- AutoPush.sh
+++ AutoPush.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export  PATH=/bin:/sbin:/usr/bin:/usr/sbin
 cd /home/harris/Desktop/Crawl_PSI
-startd=$(date); 
-echo "$startd"
+startd=$(date);
+echo "$startd Update Data"
 /usr/bin/git add .
-/usr/bin/git commit -m "$startd"
+/usr/bin/git commit -m "$startd Update Data"
 /usr/bin/git push
